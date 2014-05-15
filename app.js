1492390089e53e9cad260dd962bc6668b410b604
Travis Savo <evilgenius@nefariousplan.com>
2014-05-14 22:28:59 -0700

    Added login through google

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 var db = new Firebase('https://greatspacerace.firebaseio.com');
-var auth = new FirebaseSimpleLogin(db, function(error, user) {
-	if (error) {
-		// an error occurred while attempting login
-		console.log(error);
-	} else if (user) {
-		// user authenticated with Firebase
-		console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
-	} else {
-		// user is logged out
-	}
-});
 
-function login() {
-	auth.login('google', {
-		rememberMe : true,
-		scope : 'https://www.googleapis.com/auth/plus.login'
-	});
-}
+var GreatSpaceRace = angular.module('GreatSpaceRace', [ 'ngRoute',
+		'GreatSpaceRace.Controllers' ]);
 
-var greatSpaceRaceApp = angular.module('greatSpaceRaceApp', [ 'ngRoute',
-		'greatSpaceRaceAppControllers' ]);
+GreatSpaceRace.factory('UserService', [ function() {
+	var sdo = {
+		isLogged : "no",
+		username : ''
+	};
+	return sdo;
+} ]);
 
-greatSpaceRaceApp.config([ '$routeProvider', function($routeProvider) {
+GreatSpaceRace.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl : 'home.html'
 	}).when('/Team', {
@@ -32,8 +22,44 @@ greatSpaceRaceApp.config([ '$routeProvider', function($routeProvider) {
 	});
 } ]);
 
-var greatSpaceRaceAppControllers = angular.module('greatSpaceRaceAppControllers', []);
+var controllers = angular.module('GreatSpaceRace.Controllers', []);
 
-greatSpaceRaceAppControllers.controller('IndexCtrl', function($scope) {
+controllers.controller('LoginController', [
+		'$scope',
+		'$http',
+		'UserService',
+		function(scope, $http, UserService) {
+			scope.isLogged = false;
+			var auth = new FirebaseSimpleLogin(db, function(error, user) {
+				if (error) {
+					console.log(error);
+				} else if (user) {
+					// user authenticated with Firebase
+					console.log('User ID: ' + user.uid + ', Provider: '
+							+ user.provider);
+					// successful login
+					UserService.isLogged = "yes";
+					UserService.username = user.displayName;
+					scope.$apply();
+				} else {
+					UserService.isLogged = "no";
+					UserService.username = '';
+					scope.$apply();
+				}
+			});
+			scope.login = function() {
+				UserService.isLogged = "waiting";
+				auth.login('google', {
+					rememberMe : true,
+					scope : 'https://www.googleapis.com/auth/plus.login'
+				});
+			}
+			scope.logout = function() {
+				auth.logout();
+			}
+			scope.UserService = UserService;
+		} ]);
+
+controllers.controller('IndexCtrl', function($scope) {
 
 });
